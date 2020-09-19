4e616c43eee706273f8f3f7f0c5069f166714ecb
Melat <49922671+MelatAssefa@users.noreply.github.com>
2020-09-19 01:02:35 -0400

    Add files via upload

---
+++ A3.swift
@@ -0,0 +1,12 @@
+var integerExample : Int
+var string123 : String
+var float_Example : Float
+var _booleanExample : Bool
+var 你好arrayExample = [Int] ()
+var dictionaryExample = [Int: String]()
+
+var x = 6
+var y = "5"
+var a = Int(y) ?? 0
+var z = x + a
+print (z)
