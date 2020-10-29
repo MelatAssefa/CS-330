d5421c0d9b599a13665b80a5b8662f1d701ea8f7
Melat <49922671+MelatAssefa@users.noreply.github.com>
2020-10-29 19:32:47 -0400

    Add files via upload

---
+++ A6.swift
@@ -0,0 +1,44 @@
+print("\n")
+//For Loop
+print("Change x within a for loop\n")
+
+var x = 5
+
+for _ in (0...2) {
+  var x = 10
+  print(x)}
+
+print(x)
+print("\n")
+
+//Function 
+print("Change y within a function\n")
+
+var y = 5
+
+func new(no1: Int) {
+   var y = 10
+   print(y)
+}
+
+new(no1:y)
+
+print(y)
+print("\n")
+
+class number3 {
+	var stringEX: String = "Done"
+	func check() {
+		let number3.stringEX = "Not Done"}
+	print(stringEX)
+	check()
+	print(stringEX)
+}
+
+var a: [String] = ["c", "a", "t"]
+var b: [String] = ["d", "o", "g"]
+a=b 
+b[1] = "u"
+print(a)
+print(b) 
+
